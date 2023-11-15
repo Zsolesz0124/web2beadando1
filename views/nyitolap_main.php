6c8894c6e2f8e656cc166e63692b42b56af6118d
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-11-15 23:19:24 +0100

    nyitolap és belépés
    A nyitólapot nagyjából megcsináltam, megcsináltam a belépéshez illetve regisztrációhoz szükséges adatbázis táblákat

---
+++ views/nyitolap_main.php
@@ -0,0 +1,36 @@
+<main>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+        <div class="col-md-6 p-lg-5 mx-auto my-5">
+            <h1 class="display-3 fw-bold">Üdvözlünk a pizzériánk weboldalán!</h1>
+            <h3 class="fw-normal text-muted mb-3">Pizzák jó áron, jó helyen</h3>
+            <div class="d-flex gap-3 justify-content-center lead fw-normal">
+            </div>
+        </div>
+        <div class="product-device shadow-sm d-none d-md-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+
+    <main>
+        <div class="container py-4">
+
+            <div class="row align-items-md-stretch">
+                <div class="col-md-6">
+                    <div class="h-100 p-5 text-bg-dark rounded-3">
+                        <h2>Minőségi pizzák</h2>
+                        <p>Itt, ahol az olasz hagyományok és a szenvedély találkoznak, minden szelet egy utazás a kulináris élvezetek birodalmában. ZS 'n D csapatával együtt büszkén hoztuk létre ezt a pizzériát, ahol a friss alapanyagok, a kreatív receptek és a családias hangulat egyesül.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+                        <h2>Pizza nektek, értetek</h2>
+                        <p>A tészták szívünk és lelkünk. Pizzáink frissessége, ropogós tésztánk és gazdag ízek kombinációja garantálja, hogy minden egyes falat egy kulináris kaland lesz. Nem csupán pizzák, hanem szeretettel készített műalkotások, melyek minden szelettel tisztelegnek az olasz konyha iránt.</p>
+                    </div>
+                </div>
+            </div>
+
+            <footer class="pt-3 mt-4 text-body-secondary border-top text-center">
+                Szervezz családi összejövetelt, baráti találkozót, vagy egyszerűen jöjj el egy gyors ebédre - mindig friss és minőségi pizzákkal várunk! +36709483766
+            </footer>
+        </div>
+    </main>
+</main>
--- web2.sql
+++ web2.sql
@@ -25,6 +25,82 @@ USE `web2`;
 
 -- --------------------------------------------------------
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+                                `id` int(10) UNSIGNED NOT NULL,
+                                `csaladi_nev` varchar(45) NOT NULL DEFAULT '',
+                                `utonev` varchar(45) NOT NULL DEFAULT '',
+                                `bejelentkezes` varchar(12) NOT NULL DEFAULT '',
+                                `jelszo` varchar(40) NOT NULL DEFAULT '',
+                                `jogosultsag` varchar(3) NOT NULL DEFAULT '_1_'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- A tábla adatainak kiíratása `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `utonev`, `bejelentkezes`, `jelszo`, `jogosultsag`) VALUES
+                                                                                                         (1, 'Rendszer', 'Admin', 'Admin', sha1('admin'), '__1'),
+                                                                                                         (2, 'Családi_2', 'Utónév_2', 'Login2', sha1('login2'), '_1_');
+
+----------------------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `menu`
+--
+
+CREATE TABLE `menu` (
+                        `url` varchar(30) NOT NULL,
+                        `nev` varchar(30) NOT NULL,
+                        `szulo` varchar(30) NOT NULL,
+                        `jogosultsag` varchar(3) NOT NULL,
+                        `sorrend` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- A tábla adatainak kiíratása `menu`
+--
+
+INSERT INTO `menu` (`url`, `nev`, `szulo`, `jogosultsag`, `sorrend`) VALUES
+                                                                         ('admin', 'Admin', '', '001', 80),
+                                                                         ('belepes', 'Belépés', '', '100', 60),
+                                                                         ('elerhetoseg', 'Pizzáink', '', '111', 20),
+                                                                         ('kilepes', 'Kilépés', '', '011', 70),
+                                                                         ('nyitolap', 'Nyitólap', '', '111', 10);
+
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `menu`
+--
+ALTER TABLE `menu`
+    ADD PRIMARY KEY (`url`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
