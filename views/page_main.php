8faa0e14408949e953be24b6654d089508003dd6
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-11-17 19:45:14 +0100

    headerbe kiiratas
    Headerbe ki van írva a bejelentkezett felhasználó ha be van jelentkezve

--- views/page_main.php
+++ views/page_main.php
@@ -15,6 +15,9 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
+            <?php if($_SESSION['userid']) {?>
+                <label><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname']."
+                            ".$_SESSION['userloginname']."" ?></label><?php } ?>
         </div>
     </div>
 </nav>
